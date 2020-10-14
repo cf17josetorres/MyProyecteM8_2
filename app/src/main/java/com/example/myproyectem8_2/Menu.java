1be548cb61fd03e1ee39de3a275d2730f7907ab0
cf17jose.torres <X3Mac2qb>
2020-10-14 19:19:06 +0200

    Inici

---
+++ app/src/main/java/com/example/myproyectem8_2/Menu.java
@@ -0,0 +1,34 @@
+package com.example.myproyectem8_2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class Menu extends Fragment {
+
+    public Menu() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View fMenu = inflater.inflate(R.layout.fragment_menu, container, false);
+        Button btnAfegir = fMenu.findViewById(R.id.btnAfegir);
+        btnAfegir.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                Log.i("proves", "buttons afegir click");
+            }
+        });
+
+        return fMenu;
+    }
+}
