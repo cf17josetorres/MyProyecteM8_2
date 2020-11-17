75e4da45df0b88af52fec88caedef1ccc9c24716
cf17josetorres <cf17jose.torres@iesjoandaustria.org>
2020-11-17 04:30:44 +0100

    Terminando RV

--- app/src/main/java/com/example/myproyectem8_2/RVAdapter.java
+++ app/src/main/java/com/example/myproyectem8_2/RVAdapter.java
@@ -1,30 +1,59 @@
 package com.example.myproyectem8_2;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
 
 import java.util.ArrayList;
 
-public class RVAdapter extends RecyclerView.Adapter {
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ViewHolder> {
 
-    public RVAdapter(llistaincidencia llistaincidencia, ArrayList<incidencia> listado) {
+    private ArrayList<incidencia> array;
+    private llistaincidencia context;
+
+    public RVAdapter(llistaincidencia con, ArrayList<incidencia> arrN) {
+        array = arrN;
+        context = con;
     }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_list, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView etiquetaTitol;
+        TextView etiquetaUrgencia;
+        ConstraintLayout layout;
 
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            etiquetaTitol = itemView.findViewById(R.id.itemlistadoIncidencia);
+            etiquetaUrgencia = itemView.findViewById(R.id.itemlistadourgencia);
+            layout = itemView.findViewById(R.id.layout);
+        }
+
+
+    }
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.etiquetaTitol.setText(array.get(position).getTitol());
+        holder.etiquetaUrgencia.setText(array.get(position).getUrgencia());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return array.size();
     }
 }
